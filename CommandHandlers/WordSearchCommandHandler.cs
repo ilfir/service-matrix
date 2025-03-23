88a8cc3e349814e28a1dda4f3149c13e794682f2
ilfir <ilfir@yahoo.com>
2025-03-23 16:03:41 -0500

    Finding some words, missing for sure

--- CommandHandlers/WordSearchCommandHandler.cs
+++ CommandHandlers/WordSearchCommandHandler.cs
@@ -1,7 +1,9 @@
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using service_matrix.Helpers;
 
 public class WordSearchCommandHandler
 {
@@ -9,38 +11,67 @@ public class WordSearchCommandHandler
     {
         Stopwatch stopwatch = new Stopwatch();
         stopwatch.Start();
-
-
         List<string> definitionWords = new List<string>();
         string filePath = Path.Combine(AppContext.BaseDirectory, "resources", "definitions.txt");
         foreach (string line in File.ReadLines(filePath))
         {
+            if(line.Length > command.MaxLength || line.Length < command.MinLength)
+            {
+                continue;
+            }
             definitionWords.Add(line);
         }
+        definitionWords.Add("test");
 
         stopwatch.Stop();
         double elapsedSeconds = stopwatch.Elapsed.TotalSeconds;
         var resp = $"Time taken: {elapsedSeconds} seconds, read {definitionWords.Count} words";
-        // System.Console.WriteLine($"Time taken: {elapsedSeconds} seconds");
-// return resp;
+
+        int rows = command.lettersMatrix.Count;
+        int columns = command.lettersMatrix[0].Count;
+        string[,] lettersMatrix2D = new string[rows, columns];
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                lettersMatrix2D[i, j] = command.lettersMatrix[i][j];
+            }
+        }
+        
+        var foundWordsString = new StringBuilder("Found words: ");
+        foreach (var definitionWord in definitionWords)
+        {
+            var searchHelper = new WordSearchHelper(definitionWord, lettersMatrix2D);
+            if(searchHelper.IsAllLettersInMatrix(lettersMatrix2D, definitionWord) == false)
+            {
+                continue;
+            }
+            var searchResult = searchHelper.Search();
+            if (searchResult == true)
+            {
+                var foundWord = searchHelper.GetFoundString();
+
+                if (string.Equals(definitionWord, foundWord, StringComparison.OrdinalIgnoreCase))
+                {
+                    foundWordsString.Append(foundWord);
+                    foundWordsString.Append(", Coordinates: ");
+
+                    foreach (var wordEntry in searchHelper.GetFoundWord())
+                    {
+                        foreach (var letterEntry in wordEntry.Value)
+                        {
+                            foundWordsString.Append($"{letterEntry.Key} at {letterEntry.Value}, ");
+                        }
+                    }
+                    foundWordsString.Append(" | ");
+                }
+
+                resp = foundWordsString.ToString().TrimEnd(',', ' ');
+                // break;
+            }
+        }
 
 
-        // Implement your word search logic here
-        List<string> foundWords = new List<string>();
-
-        // foundWords.Add(resp);
-
-        // // Example logic (replace with actual implementation)
-        // foreach (var row in command.lettersMatrix)
-        // {
-        //     foreach (var letter in row)
-        //     {
-        //         if (letter.Length >= command.MinLength && letter.Length <= command.MaxLength)
-        //         {
-        //             foundWords.Add(letter);
-        //         }
-        //     }
-        // }
 
         return Task.FromResult(resp);
     }
---
+++ Helpers/WordSearchHelper.cs
@@ -0,0 +1,256 @@
+using System.Text;
+
+namespace service_matrix.Helpers
+{
+    public class WordSearchHelper
+    {
+        private string[,] _arLetters;
+       
+        private readonly string[,] _arLettersStatic;
+        private readonly string[] _arWord;
+        private Dictionary<int, Dictionary<string, string>> _foundWord;
+        private int _iFirstColumn = 0;
+        private int _iFirstRow = 0;
+        private StringBuilder _sFoundString;
+        private readonly string _sWord;
+
+        public WordSearchHelper(string sWord2, string[,] arLetters2)
+        {
+            _arLettersStatic = CopyArray(arLetters2);
+            _arLetters = CopyArray(_arLettersStatic);
+            _foundWord = new Dictionary<int, Dictionary<string, string>>();
+            _sFoundString = new StringBuilder();
+            _arWord = sWord2.ToCharArray().Select(c => c.ToString()).ToArray();
+            _sWord = sWord2;
+        }
+        
+        public bool Search()
+        {
+            return FindWord(null, null, null);
+        }
+
+        public Dictionary<int, Dictionary<string, string>> GetFoundWord()
+        {
+            return _foundWord;
+        }
+
+        public string GetFoundString()
+        {
+            return _sFoundString.ToString();
+        }
+        
+        public bool FindWord(int? iWordIndex, int? iMatrixStart, int? jMatrixStart)
+        {
+            if (iMatrixStart == null)
+            {
+                iMatrixStart = 0;
+            }
+            if (jMatrixStart == null)
+            {
+                jMatrixStart = 0;
+            }
+            if (iWordIndex == null)
+            {
+                iWordIndex = 0;
+            }
+            for (int w = iWordIndex.Value; w < _arWord.Length && iWordIndex.Value <= _foundWord.Count; w++)
+            {
+                iWordIndex = w;
+                string sSearchChar = _arWord[w];
+                bool bFound = false;
+                for (int i = iMatrixStart.Value; i < _arLetters.GetLength(0) && !bFound; i++)
+                {
+                    for (int j = jMatrixStart.Value; j < _arLetters.GetLength(1) && !bFound; j++)
+                    {
+                        if (sSearchChar.Equals(_arLetters[i, j], StringComparison.OrdinalIgnoreCase) && IsNeighborToPrevLetter(i, j, iWordIndex.Value, sSearchChar))
+                        {
+                            if (IsNeighborToNextLetter(i, j, _arWord, iWordIndex.Value, CopyArray(_arLetters)))
+                            {
+                                var hLetLoc = new Dictionary<string, string>
+                                {
+                                    { sSearchChar, $"{i} {j}" }
+                                };
+                                _foundWord[iWordIndex.Value] = hLetLoc;
+                                _sFoundString.Append(_arLetters[i, j]);
+                                _arLetters[i, j] = "*";
+                                bFound = true;
+                                // return true;
+                            }
+                        }
+                    }
+                }
+            }
+            if (!string.IsNullOrEmpty(_sFoundString.ToString()) || _sFoundString.ToString().Equals(_sWord, StringComparison.OrdinalIgnoreCase))
+            {
+                return true;
+            }
+            int tempCol = _iFirstColumn;
+            int tempRow = _iFirstRow;
+            UpdateNextFirstLetterStartPos();
+            if (!(tempCol == _iFirstColumn && tempRow == _iFirstRow) && GetNextFirstLetter(_iFirstColumn, _iFirstRow))
+            {
+                FindWord(1, null, null);
+            }
+            return false;
+        }
+
+        public bool GetNextFirstLetter(int iMatrixStart, int jMatrixStart)
+        {
+            int i = _iFirstColumn;
+            int i2 = _iFirstRow;
+            string sSearchChar = _arWord[0];
+            bool bFound = false;
+            _arLetters = CopyArray(_arLettersStatic);
+            _foundWord = new Dictionary<int, Dictionary<string, string>>();
+            _sFoundString = new StringBuilder();
+            for (int i3 = iMatrixStart; i3 < _arLetters.GetLength(0) && !bFound; i3++)
+            {
+                int j = jMatrixStart;
+                while (true)
+                {
+                    if (j >= _arLetters.GetLength(1) || bFound)
+                    {
+                        break;
+                    }
+                    if (sSearchChar.Equals(_arLetters[i3, j]))
+                    {
+                        if (IsNeighborToNextLetter(i3, j, _arWord, 0, CopyArray(_arLetters)))
+                        {
+                            _sFoundString.Append(sSearchChar);
+                            var hLetLoc = new Dictionary<string, string>
+                            {
+                                { sSearchChar, $"{i3} {j}" }
+                            };
+                            _foundWord[0] = hLetLoc;
+                            _arLetters[i3, j] = "*";
+                            bFound = true;
+                            break;
+                        }
+                    }
+                    j++;
+                }
+                jMatrixStart = 0;
+            }
+            return bFound;
+        }
+
+        public string[,] CopyArray(string[,] source)
+        {
+            string[,] copy = new string[source.GetLength(0), source.GetLength(1)];
+            for (int i = 0; i < source.GetLength(0); i++)
+            {
+                for (int j = 0; j < source.GetLength(1); j++)
+                {
+                    copy[i, j] = source[i, j];
+                }
+            }
+            return copy;
+        }
+
+        private void UpdateNextFirstLetterStartPos()
+        {
+            int tempCol = _iFirstColumn;
+            int tempRow = _iFirstRow;
+            if (_iFirstColumn < 5 && _iFirstRow < 5)
+            {
+                _iFirstRow++;
+                if (_iFirstRow > 4)
+                {
+                    _iFirstRow %= 4;
+                    _iFirstColumn++;
+                }
+                if (_iFirstColumn > 4 || _iFirstRow > 4)
+                {
+                    _iFirstColumn = tempCol;
+                    _iFirstRow = tempRow;
+                }
+            }
+        }
+
+        private bool IsNeighborToNextLetter(int iCurrentX, int iCurrentY, string[] arWord2, int iWordIndex, string[,] arLettersLoc)
+        {
+            if (iWordIndex == arWord2.Length - 1 || iWordIndex == 0)
+            {
+                return true;
+            }
+            string sNextLetter = arWord2[iWordIndex + 1];
+            for (int dX = 1; dX <= 3; dX++)
+            {
+                for (int dY = 1; dY <= 3; dY++)
+                {
+                    int neighborX = (iCurrentX + dX) - 2;
+                    int neighborY = (iCurrentY + dY) - 2;
+                    if (!(neighborX == iCurrentX && neighborY == iCurrentY) && neighborX >= 0 && neighborX <= 4 && neighborY >= 0 && neighborY <= 4 && sNextLetter.Equals(arLettersLoc[neighborX, neighborY]))
+                    {
+                        bool secondNextNeighbor = true;
+                        if (arWord2.Length > iWordIndex + 2)
+                        {
+                            string[,] arLettersLocTemp = CopyArray(arLettersLoc);
+                            arLettersLocTemp[iCurrentX, iCurrentY] = "*";
+                            secondNextNeighbor = IsNeighborToNextLetter(neighborX, neighborY, arWord2, iWordIndex + 1, arLettersLocTemp);
+                        }
+                        if (secondNextNeighbor)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        private bool IsNeighborToPrevLetter(int iCol, int iRow, int iWordIndex, string sLet)
+        {
+            if (iWordIndex == 0)
+            {
+                _iFirstColumn = iCol;
+                _iFirstRow = iRow;
+                return true;
+            }
+            if (!_foundWord.TryGetValue(iWordIndex - 1, out var hLetterIdx))
+            {
+                return false;
+            }
+            string value = "";
+            foreach (var k in hLetterIdx.Keys)
+            {
+                value = hLetterIdx[k];
+                break;
+            }
+            string[] sIndex = value.Split(' ');
+            int iPrevRow = int.Parse(sIndex[1]);
+            int xDelta = Math.Abs(Math.Abs(int.Parse(sIndex[0])) - Math.Abs(iCol));
+            int yDelta = Math.Abs(Math.Abs(iPrevRow) - Math.Abs(iRow));
+            return xDelta < 2 && yDelta < 2;
+        }
+        
+        /// <summary>
+        /// Determine whether all letter of the given word are in the matrix
+        /// </summary>
+        /// <param name="matrix"></param>
+        /// <param name="wholeWord"></param>
+        /// <returns></returns>
+        public bool IsAllLettersInMatrix(String[,] matrix, String wholeWord) {
+            
+            List<char>? _allArrayLetters = new();
+            var word = wholeWord.ToCharArray();
+        
+            _allArrayLetters = new List<char>();
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    _allArrayLetters.Add(matrix[i, j].ToCharArray()[0]);
+                }
+            }
+            
+            foreach (var c in word)
+            {
+                if (!_allArrayLetters.Contains(c)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
