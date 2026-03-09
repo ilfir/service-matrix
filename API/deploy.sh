3b897d85376773e43a7b3fdf925c946cb09d8640
ilfir <ilfir@yahoo.com>
2026-03-09 10:05:43 -0500

    UPdate deploy script remove dupe

--- API/deploy.sh
+++ API/deploy.sh
@@ -1,2 +1,2 @@
 docker build --no-cache -t service-matrix .
-docker run -p 8080:8080  -v /Users/ilfir/service-matrix-data:/app/data service-matrix
+docker run --name service-matrix -p 8080:8080 service-matrix &
